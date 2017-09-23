280f7220194c2ddf4173fc76d59bdb1bb1514ee4
hyy044101331 <hyy044101331@163.com>
2017-09-23 21:34:01 +0800

    创建简单的菜单

--- spring-mvc-easyui/src/main/java/com/mengka/controller/InnoController.java
+++ spring-mvc-easyui/src/main/java/com/mengka/controller/InnoController.java
@@ -20,6 +20,14 @@ public class InnoController {
     @Autowired
     private UserManager userManager;
 
+    /**
+     *  CURD应用
+     *
+     * @param map
+     * @param request
+     * @param groupName
+     * @return
+     */
     @RequestMapping(value = "/aa.do", method = {RequestMethod.GET, RequestMethod.POST})
     public String topic(ModelMap map, HttpServletRequest request,
                         @RequestParam(required = false) String groupName) {
@@ -27,15 +35,60 @@ public class InnoController {
         return "inno/aa";
     }
 
+    /**
+     *  region边框布局：east、west、north、south、center
+     *
+     * @param map
+     * @param request
+     * @param groupName
+     * @return
+     */
     @RequestMapping(value = "/bb.do", method = {RequestMethod.GET, RequestMethod.POST})
     public String bb(ModelMap map, HttpServletRequest request,
                         @RequestParam(required = false) String groupName) {
         return "inno/bb";
     }
 
+    /**
+     *  布局：折叠面板、标签页
+     *
+     * @param map
+     * @param request
+     * @param groupName
+     * @return
+     */
     @RequestMapping(value = "/cc.do", method = {RequestMethod.GET, RequestMethod.POST})
     public String cc(ModelMap map, HttpServletRequest request,
                      @RequestParam(required = false) String groupName) {
         return "inno/cc";
+    }
+
+    /**
+     *  复杂的表头
+     *
+     * @param map
+     * @param request
+     * @param groupName
+     * @return
+     */
+    @RequestMapping(value = "/table_01.do", method = {RequestMethod.GET, RequestMethod.POST})
+    public String table_01(ModelMap map, HttpServletRequest request,
+                     @RequestParam(required = false) String groupName) {
+        map.put("list", userManager.initUserData());
+        return "inno/table_01";
+    }
+
+    /**
+     *  菜单
+     *
+     * @param map
+     * @param request
+     * @param groupName
+     * @return
+     */
+    @RequestMapping(value = "/menu_01.do", method = {RequestMethod.GET, RequestMethod.POST})
+    public String menu_01(ModelMap map, HttpServletRequest request,
+                           @RequestParam(required = false) String groupName) {
+        return "inno/menu_01";
     }
 }
