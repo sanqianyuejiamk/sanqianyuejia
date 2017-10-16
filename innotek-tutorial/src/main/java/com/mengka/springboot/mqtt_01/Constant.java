93d63534e8efc25400394317b2e4ac746cd2f149
hyy044101331 <hyy044101331@163.com>
2017-10-16 22:18:16 +0800

    MQTT协议

---
+++ innotek-tutorial/src/main/java/com/mengka/springboot/mqtt_01/Constant.java
@@ -0,0 +1,20 @@
+package com.mengka.springboot.mqtt_01;
+
+/**
+ * @author huangyy
+ * @date 2017/10/16.
+ */
+public class Constant {
+
+    public static final String hostname = "tcp://127.0.0.1:61613";
+
+    public static final String uuid = "admin";
+    public static final String token = "password";
+
+    public static final String username = uuid;
+    public static final String password = token;
+
+    public static final String subscribeTopic = "aa-topic";
+
+    public static final String publisheTopic = "aa-topic";
+}
