c7c499515bb6314c0f49229e11d215530e326487
hyy044101331 <hyy044101331@163.com>
2017-10-20 22:00:00 +0800

    nb-iot

---
+++ innotek-tutorial/src/main/java/com/mengka/springboot/nbiot_01/nbiot_01.java
@@ -0,0 +1,23 @@
+package com.mengka.springboot.nbiot_01;
+
+/**
+ * NB-IoT
+ *
+ * NarrowBand IoT (NB-IoT) is a Low Power Wide Area Network (LPWAN) radio technology
+ * standard developed to enable a wide range of devices
+ * and services to be connected using cellular telecommunications bands.
+ *
+ * NB-IoT is a narrowband radio technology designed for the Internet of Things (IoT)
+ * and is one of a range of Mobile IoT (MIoT) technologies
+ * standardized by the 3rd Generation Partnership Project (3GPP).
+ *
+ * Other 3GPP IoT technologies
+ * 1)NB-IoT;
+ * 2)eMTC (enhanced Machine-Type Communication);
+ * 3)EC-GSM-IoT;
+ *
+ * @author huangyy
+ * @date 2017/10/20.
+ */
+public class nbiot_01 {
+}
